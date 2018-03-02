11a04780e6386642581799640b320b23eb317d74
Heidi Hlavinka <hchlav@gmail.com>
2018-03-02 17:14:34 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -2,14 +2,13 @@ def unsafe?(speed)
 	if speed > 60
 		return true
 	elsif speed < 40
-		return true	
+		return true
 	elsif speed != speed.between?(40, 60)
 		return false
   end
 end
 
 def not_safe?(speed)
-	speed == ? true : false
+	speed < 40 || speed > 60 ? true : false
 
 end
-
